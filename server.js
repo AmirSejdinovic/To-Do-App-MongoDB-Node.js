19eb6a8990e621fb6370571ecf1b0a324e056746
Amir Sejdinovic <amir24V@yahoo.com>
2019-12-25 13:07:41 +0100

    Extracting the value from input field

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 let express = require('express');
 //Calling the express
 let app = express();
+//This configures express so the express can add input value inside the body of request
+app.use(express.urlencoded({extended: false}));
 //What will app do when recives the get request on the home page. When user request the homepage with get request then this code triger the anonimus function which have 2 parameters and that is request and resposne
 app.get('/', function(req,res){
   //This will display the code block when someone visits our homepage. Here i put the html which will be loaded when someone visit our website. I put this html into the template literals because I want to write inside of it the js code. Here I use the bootstrap for nice UX
@@ -20,9 +22,9 @@ app.get('/', function(req,res){
     <h1 class="display-4 text-center py-1">To-Do App</h1>
     
     <div class="jumbotron p-3 shadow-sm">
-      <form>
+      <form action="create-item" method="POST">
         <div class="d-flex align-items-center">
-          <input autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+          <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
           <button class="btn btn-primary">Add New Item</button>
         </div>
       </form>
@@ -59,6 +61,12 @@ app.get('/', function(req,res){
 
 
   `);
+})
+//When the browser send the post request with /create-item than do this function. This create item is the action of form
+app.post('/create-item', function(req, res){
+  //With this req.body.item I pickup the input value and I can use it anywhere I want. "item" is the name proprety of input inside html template
+   console.log(req.body.item);
+   res.send("Thank you for form");
 })
 
 //Listening the por 300
