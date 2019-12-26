6a66aa78226e365718b329d6e110186366940d48
Amir Sejdinovic <amir24V@yahoo.com>
2019-12-26 14:41:10 +0100

    Deleting the todos

--- public/browser.js
+++ public/browser.js
@@ -1,5 +1,16 @@
 //Here I setup the addEventListener to the document
 document.addEventListener("click", function(e){
+  //DELETE FEATURE
+  if(e.target.classList.contains("delete-me")){
+     if(confirm("Do you really want to delete this item permanently?")){
+        axios.post('/delete-item', {id: e.target.getAttribute("data-id") }).then(function(){
+          e.target.parentElement.parentElement.remove();
+        }).catch();
+     }
+  }
+  
+  
+  //UPDATE
   //Here I check if the click is happend on the element which contains the class "edit-me", than do the code. Every function in the addEventListener have parametar which contains all sorts of values one of them is the target
   if(e.target.classList.contains("edit-me")){
     //This is the default browser method where user can type the text
